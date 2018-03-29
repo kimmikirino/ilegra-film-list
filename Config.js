983184b37e7e665e5a3038a51d01573e1ccb8cad
Kimmi Kirino <bfatskih@gmail.com>
2018-03-29 17:44:16 -0300

    Built ilegra from commit 03c6958 on branch master

--- Config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var appConfig = angular.module('appConfig', []);
-
-appConfig.constant('basePath', '');
-
-appConfig.constant('ilegraConfig', {
-    endpoint: 'https://swapi.co/api/films'
-});
